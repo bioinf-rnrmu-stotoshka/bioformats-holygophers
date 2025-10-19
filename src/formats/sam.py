afb545242757e5e33b6e62133977342aa6a10d62
kyberdog <adeptusshaos@gmail.com>
2025-10-19 18:11:06 +0300

    Update sam.py

--- src/formats/sam.py
+++ src/formats/sam.py
@@ -1,8 +1,42 @@
-"""
-SAM format processor.
-To be implemented by colleague.
-"""
+class Samreader:
+    def __init__(self, filename):
+        self.filename = filename
+        self.header = []
+        self.levels = []
 
-class SamProcessor:
-    """SAM format processor - implementation pending."""
-    pass
+    def read(self):
+        with open(self.filename, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line.startswith('@'):
+                    obj = line[1:3]
+                    if obj not in self.header:
+                        self.header[obj] = []
+                    self.header[obj].append(line)
+                else:
+                    fields = line.split('\t')
+                    level = {
+                        'QNAME': fields[0],
+                        'FLAG': int(fields[1]),
+                        'RNAME': fields[2],
+                        'POS': int(fields[3]),
+                        'MAPQ': int(fields[4]),
+                        'CIGAR': fields[5],
+                        'SEQ': fields[9],
+                        'QUAL': fields[10]
+                    }
+                    self.levels.append(level)
+                    yield level
+
+def getheader(self):
+    return self.header
+
+def sortlevels(self, flagmask):
+    return (lvl for lvl in self.levels if (lvl ['FLAG'] and flagmask) == flagmask)
+
+def countlvlsperchrom(self):
+    counts = {}
+    for lvl in self.levels:
+        chrom = lvl['RNAME']
+        counts[chrom] = counts.get(chrom, 0)+1
+    return counts
